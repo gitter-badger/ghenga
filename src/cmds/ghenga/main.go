0b61a4a4a4f29a17f01dcaea5bd6e57c3a1b6c61
Alexander Neumann <alexander@bumpern.de>
2016-04-22 22:54:21 +0200

    Code

--- src/cmds/ghenga/main.go
+++ src/cmds/ghenga/main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandlePerson(res http.ResponseWriter, req *http.Request) {
 }
 
+func HandleDefault(res http.ResponseWriter, req *http.Request) {
+	log.Printf("default handler called")
+}
+
 func main() {
 	r := mux.NewRouter()
-	// r.HandleFunc("/search/{searchTerm}", Search)
-	// r.HandleFunc("/load/{dataId}", Load)
 	r.HandleFunc("/api/person", HandlePerson)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("public")))
-	http.Handle("/", r)
+
+	http.Handle("/", handlers.CombinedLoggingHandler(os.Stderr, r))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
